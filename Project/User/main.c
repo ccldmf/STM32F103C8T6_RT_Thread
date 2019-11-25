9f6b2b2a28c2df2b0e0aa53e927b037f9d68a633
Balder <13568859409@163.com>
2019-11-25 22:24:48 +0800

    Update user application
    Update user application
    
    Change-Id: Ie97028ffed208e1b4e76c5b91a280fc8d5a44e9a
    Type: Task

--- Project/User/main.c
+++ Project/User/main.c
@@ -20,6 +20,7 @@
 #include "usart.h"
 #include "esp8266.h"
 #include "bigiot.h"
+#include "dht11.h"
 #include <stdio.h>
 
 /**
@@ -29,16 +30,19 @@
   */
 int main(void)
 {
+    uint32_t theHighValue,theLowValue;
+    uint32_t theTmp;
     SystemInit();
     LED_Init();
     Delay_Init();
     USART_Configuration(USART1,115200);
     USART_Configuration(USART2,115200);
     USART_Configuration(USART3,115200);
+    DHT11_Init();
     printf("************************************\n");
     printf("*          Smart House Iinit       *\n");
     printf("************************************\n");
-    BigiotLogin("6757","a215adb24");
+
 
     while (1)
     {
@@ -47,8 +51,12 @@ int main(void)
         Delay_ms(100);
         LED_Off();
         Delay_ms(100);
-        Delay_ms(2000);
-        BigiotSendData("6757","6033","33.5");
+        DHT11_ReadData();
+        theTmp = DHT11_GetTem();
+        printf("Tem = %d.%d\n" , (theTmp>>8) , (theTmp&0xFF));
+        theTmp = DHT11_GetHum();
+        printf("Hum = %d.%d\n" , (theTmp>>8) , (theTmp&0xFF));
+        Delay_ms(1500);
     }
 }
 
